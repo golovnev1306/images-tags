37d32385127a8baddece5f38cc578c60e7bf957f
Constantine <golovnev1306@mail.ru>
2021-07-25 11:40:45 +0700

    init app, install packages

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.scss'
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+          Learn React
+      </header>
+    </div>
+  )
+}
+
+export default App
